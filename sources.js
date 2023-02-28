39df28d7a70df32b88f4f3336e7c555214eaa403
Hasan Burak Songur <hasanburaksongur@gmail.com>
2023-02-28 09:40:46 +0300

    define the algorithm steps

---
+++ sources.js
@@ -0,0 +1,6 @@
+//create a container that will contain grid
+//make it use flex item
+//create a loop for creating a grid square
+//append the container
+//add a hover to squares
+//create a button to ask user for grid size
